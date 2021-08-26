58fc92dcfdd322d4f4b22fd91759a757b9493a40
Dundaki <vigjoaopaulo.com>
2021-08-26 07:44:10 -0300

    response_code recebe code = 200

--- app/src/main/java/br/com/webserviceteste/model/BuscarIp.java
+++ app/src/main/java/br/com/webserviceteste/model/BuscarIp.java
@@ -1,24 +1,10 @@
 package br.com.webserviceteste.model;
 
-import static android.content.Context.WIFI_SERVICE;
-
-import android.content.Context;
-import android.net.wifi.WifiManager;
-import android.util.Log;
-
 import java.net.InetAddress;
+import java.net.SocketException;
 import java.net.UnknownHostException;
-import java.util.Formatter;
 
 public class BuscarIp {
-
-
-    public  void getIps() throws UnknownHostException {
-
-
-    }
-
-
 
 
 }
--- app/src/main/java/br/com/webserviceteste/model/ListarEstadosAsyncTask.java
+++ app/src/main/java/br/com/webserviceteste/model/ListarEstadosAsyncTask.java
@@ -5,69 +5,148 @@ import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.InetAddress;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.UnknownHostException;
 
 public class ListarEstadosAsyncTask extends  AsyncTask<String, String, String> {
-    String ip = "10.0.0.108";
+    String ip = "10.0.0.109";
     String ip2 = "192.168.1.108";
-    String caminho = "http://ip2/curso_udemy/exer/APIListarCidades.php";
+    String query, caminho = "http://"+ip2+"/curso_udemy/exer/APIListarEstados.php";
     String api_token;
     HttpURLConnection conn;
     URL url = null;
     Uri.Builder builder;
-    String p ;
-
-
-   private static String getIp(){
-       String host,name;
-       try{
-           host = InetAddress.getLocalHost().getHostAddress();
-
-       }catch (UnknownHostException e ){
-           host = "Unknow";
-       }
-       return host;
-
-   }
-
-
-
+    int response_code;
     final  int READ_TIME_OUT = 10000;
     final  int CONNECTION_TIME_OUT = 30000;
 
     final String URL_WEB_SERVICE = caminho;
 
-    public ListarEstadosAsyncTask(String token) {
-        this.api_token =  token;
+    public ListarEstadosAsyncTask(String token){
+
+        this.api_token = token;
         this.builder = new Uri.Builder();
         builder.appendQueryParameter("api_token", api_token);
 
-        int parada  = 0;
     }
 
     @Override
     protected void onPreExecute(){
 
-
-        Log.i("APIListar" , "onPreExecute()");
+        Log.i("APIListar","onPreExecute()");
 
     }
 
     @Override
     protected String doInBackground(String... strings) {
 
+        Log.i("APIListar","doInBackground()");
 
-        Log.i("APIListar", "getIp() " + getIp());
-        Log.i("APIListar" , "doInBackground()");
-        return "processado com sucesso";
+        // Gerar o conteúdo para a URL
+
+        try {
+
+            url = new URL(URL_WEB_SERVICE);
+
+        }catch (MalformedURLException e){
+
+            Log.i("APIListar","doInBackground() --> "+e.getMessage());
+
+        }catch (Exception e){
+
+            Log.i("APIListar","doInBackground() --> "+e.getMessage());
+        }
+
+        // Gerar uma requisição HTTP - POST - Result será um ArrayJson
+
+        // conn
+
+        try {
+
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(READ_TIME_OUT);
+            conn.setConnectTimeout(CONNECTION_TIME_OUT);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("charset","utf-8");
+
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+            conn.connect();
+
+        }catch (Exception e){
+
+            Log.i("APIListar","doInBackground() --> "+e.getMessage());
+
+        }
+
+        // Adicionar o TOKEN e/ou outros parâmetros como por exemplo
+        // um objeto a ser incluido, deletado ou alterado.
+        // CRUD completo
+
+        try {
+
+            query = builder.build().getEncodedQuery();
+
+            OutputStream stream = conn.getOutputStream();
+
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(stream,"utf-8"));
+
+            writer.write(query);
+            writer.flush();
+            writer.close();
+            stream.close();
+
+            conn.connect();
+
+
+        }catch (Exception e){
+
+            Log.i("APIListar","doInBackground() --> "+e.getMessage());
+
+
+        }
+
+        // receber o response - arrayJson
+        // http - código do response | 200 | 404 | 503
+
+        try {
+
+            response_code = conn.getResponseCode();
+
+
+
+        }catch (Exception e){
+
+            Log.i("APIListar","doInBackground() --> "+e.getMessage());
+        }
+
+
+        return "Processamento concluído...";
+
+
     }
 
     @Override
     protected void onPostExecute(String result){
-        Log.i("APIListar" , "onPostExecute() --> " + result);
+
+        Log.i("APIListar","onPostExecute()--> Result: "+result);
+
+
     }
 
 
